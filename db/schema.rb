45449953e7e453bf324802dea0faa3099bab7a79
Adam Moses <adammoses13@hotmail.com>
2013-06-17 21:59:19 -0600

    formatting

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130617014540) do
+ActiveRecord::Schema.define(:version => 20130618034446) do
 
-
+  create_table "pins", :force => true do |t|
+    t.string   "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "routes", :force => true do |t|
     t.string   "description"
