d4145a0edaf41c2c1a67b3f46f608e2899997237
Adam Moses <adammoses13@hotmail.com>
2013-06-18 22:26:46 -0600

    Delete Permalink

--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ActionController::Base
-  def show
-     @user = User.find_by_permalink(param[:id])
-     @title = @user.name
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
-
-  before_save :create_permalink
-  private
-     def create_permalink
-          self.permalink = name.downcase
-     end
-
-def to_param
-  permalink
-end
   
   has_many :routes, :dependent => :destroy
   has_many :to_dos, :dependent => :destroy
