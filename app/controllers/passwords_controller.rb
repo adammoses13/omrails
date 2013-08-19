7a387b3b4c2412a4d0a45bebc7e6205bc7e02b72
Adam Moses <adammoses13@hotmail.com>
2013-08-19 16:36:23 -0600

    Attempt to fix rest password form submit button

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,47 +1,28 @@
 class PasswordsController < Devise::PasswordsController
 
 
+def edit
+  @user = User.find_by_password_reset_token!(params[:id])
+  session[:user_id] = @user.id
+end
+
+
 def create
-    @user = User.find_by_email(params[:user][:email])
-    if @user
-      @user.send_password_reset
-      flash[:notice] = "E-mail has been sent, please follow link in e-mail to reset password."
-      redirect_to root_path
-    else
-      flash[:notice] = "Sorry, we couldn't find that email. Please try again."
-      redirect_to root_path
-      
-    end
-
+  @user = User.find_by_email(params[:user][:email])
+  if @user
+    @user.send_password_reset
+    flash[:notice] = "E-mail has been sent, please follow link in e-mail to reset password."
+    redirect_to root_path
+  else
+    flash[:notice] = "Sorry, we couldn't find that email. Please try again."
+    redirect_to root_path
   end
+end
 
 
-	#def create
-	#	@user = User.find_by_email(params[:user][:email])
- 		
- 	#	if @user
-  #    UserMailer.reset_password_instructions(@user).deliver
-  #    flash[:notice] = "E-mail has been sent, please follow link in e-mail to reset password."
-  #    redirect_to root_path
-    
- 	#	end
-	#end
+
+
+
+
 
 end
-#self.resource = resource_class.send_reset_password_instructions(resource_params)
-#
-  #          if successfully_sent?(resource)
-  #          flash[:notice] = "sent password"
-  #          else
-  #            respond_with(resource)
-  #          end
-  #      end
-
-
-
-#@user = User.find_by_email(params[:user][:email])
-#  respond_to do |format|
-#  UserMailer.reset_password_instructions(@user).deliver
-#        format.html { redirect_to(root_path(resource_name), notice: 'E-mail has been sent, please follow link in e-mail to reset password.') }
- #     format.json { render json: @user, status: :created, location: @user }
-# end
