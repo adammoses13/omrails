67badba645c3b376aa0232432cc964e0719b9b99
Adam Moses <adammoses13@hotmail.com>
2014-03-13 22:44:13 -0600

    update pw

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,11 +1,11 @@
 class SuggestionsController < ApplicationController
   
   def new
-    @suggestion =Suggestion.new
+    @suggestion = Suggestion.new
   end
     
   def create
-    @suggestion =Suggestion.new(params[:suggestion])
+    @suggestion = Suggestion.new(params[:suggestion])
     if @suggestion.deliver
       redirect_to root_path, notice: "Thank you!  Your suggestion has been submitted."
     else
